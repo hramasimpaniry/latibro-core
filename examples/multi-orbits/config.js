235b688a0f1d93a708118323bae8552bfa988f5c
Heritiana R. RAMASIMPANIRY <hramasimpaniry@gmail.com>
2025-01-27 20:10:33 +0300

    add example 'multi-orbits'

---
+++ examples/multi-orbits/config.js
@@ -0,0 +1,35 @@
+import Orbital from "../../src/Orbital"; // 'latibro-core'
+
+const container = document.getElementById("orbital-container");
+const config = {
+  orbits: [
+    {
+      items: [
+        "https://placehold.co/50",
+        "https://placehold.co/50",
+        "https://placehold.co/50",
+      ],
+      speed: 8,
+    },
+    {
+      items: [
+        "https://placehold.co/50",
+        "https://placehold.co/50",
+        "https://placehold.co/50",
+      ],
+      speed: 12,
+      customRadius: 80,
+    },
+    {
+      items: [
+        "https://placehold.co/50",
+        "https://placehold.co/50",
+        "https://placehold.co/50",
+      ],
+      speed: 16,
+      customRadius: 100,
+    },
+  ],
+};
+
+new Orbital(container, config);
