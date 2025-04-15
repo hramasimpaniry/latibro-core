2236fd437e60870b90018b94404f85d05071e404
Heritiana R. RAMASIMPANIRY <hramasimpaniry@gmail.com>
2025-04-15 21:30:19 +0300

    add type-setting for new variables

--- src/index.d.ts
+++ src/index.d.ts
@@ -17,19 +17,58 @@ declare module "latibro-core" {
   export interface ContainerOptions {   
     customCss?: string;
     styles?: Partial<CSSStyleDeclaration>;
-  }  
+  } 
+  
+  export interface PanelOptions {
+    container?: HTMLElement;
+    offset?: {
+      width?: number;
+      height?: number;
+    };
+    close?: {
+      label?: string;
+      title?: string;
+    };
+  }
 
   export interface OrbitalOptions {
     container?: ContainerOptions;
     orbits: OrbitOptions[];
     orbitSpacing?: number;
-  }    
-
-    export default class Orbital {
-      constructor(container: HTMLElement, options: OrbitalOptions);
-      private init(): void;
-      private createOrbits(): void;
-      private defineCSSRule(cssRules: string): void;
-    }    
+    interactive?: boolean;
+    panel?: PanelOptions;
+    mouseLeaveDelay?: number;
   }
+
+  export interface AnimationSequence {
+    duration: number;
+    properties: Record<string, {
+      from?: string;
+      to: string;
+    }>;
+    before?: (el: HTMLElement) => void;
+    after?: (el: HTMLElement) => void;    
+  }
+
+  export default class Orbital {
+    constructor(container: HTMLElement, options: OrbitalOptions);
+    private init(): void;
+    private createOrbits(): void;
+    private defineCSSRule(cssRules: string): void;
+    private animate(element: HTMLElement, sequences: AnimationSequence[]): Promise<void>;
+    private _animateMultipleProperties(
+      element: HTMLElement,
+      duration: number,
+      properties: Record<string, { 
+        from?: string; 
+        to: string 
+      }>,
+      before?: (el: HTMLElement) => void,
+      after?: (el: HTMLElement) => void
+    ): Promise<void>;
+    private openPanel(): void;
+    private closePanel(): void;
+    private setupItemInteractivity(itemData: any): void;
+  }    
+}
   
