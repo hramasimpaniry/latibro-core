147c5265eb467be792b8e50f7b187316d40255d0
Heritiana R. RAMASIMPANIRY <hramasimpaniry@gmail.com>
2025-04-16 21:06:06 +0300

    create interactivity test suite

--- tests/orbital/interactivity.test.js
+++ tests/orbital/interactivity.test.js
@@ -38,6 +38,9 @@ describe("Orbital: Interactivity", () => {
     // interactivity
     await userEvent.hover(item, { force: true });
 
+    // wait
+    await new Promise((resolve) => setTimeout(resolve, 50));
+
     // tests
     await expect.element(orbits[0]).toHaveClass("orbit-paused");
   });
@@ -64,7 +67,51 @@ describe("Orbital: Interactivity", () => {
     // interactivity
     await userEvent.unhover(item, { force: true });
 
+    // wait
+    await new Promise((resolve) => setTimeout(resolve, 50));
+
     // tests
     await expect.element(orbits[0]).not.toHaveClass("orbit-paused");
+  });
+
+  it("should open panel on item click", async () => {
+    // options
+    const options = {
+      interactivity: true,
+      orbits: [
+        {
+          items: [
+            {
+              src: "https://placehold.co/50",
+              panel: {
+                content: "Hello from Item 1",
+              },
+            },
+            "https://placehold.co/50",
+            "https://placehold.co/50",
+          ],
+          speed: 10,
+        },
+      ],
+    };
+
+    // init
+    orbital = new Orbital(container, options);
+
+    // elements
+    const orbits = container.querySelectorAll(".orbit");
+    const item = container.querySelector(".orbit .orbit-item");
+
+    // interactivity
+    await userEvent.click(item, { force: true });
+
+    // wait
+    await new Promise((resolve) => setTimeout(resolve, 1200));
+
+    // panel
+    const panel = document.querySelector(".orbit-panel");
+
+    // tests
+    await expect.element(panel).not.toBeNull();
   });
 });
